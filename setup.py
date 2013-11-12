58a21725f9249ca419c3cef7e6a98215990c0190
Panos Kittenis <pkittenis@gmail.com>
2013-11-12 15:49:04 +0000

    Updated install_requires in setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(name = 'cronify',
       author_email = 'pkittenis@gmail.com',
       url = "https://github.com/pkittenis/cronify",
       packages = find_packages('.'),
-      install_requires = ['pyinotify', 'pyaml', 'python-daemon', 'pytz'],
+      install_requires = ['pyinotify', 'PyYAML', 'python-daemon', 'pytz'],
       classifiers = [
         'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
         'Intended Audience :: Developers',
